71d368622b8cc75a9495c03b5c158d19c86deca6
Svitlana Rybakova <rsv0011@gmail.com>
2024-10-30 16:03:43 +0100

    feat: create the vehicle edit view

--- Garage2.0/Models/ViewModels/VehicleEditViewModel.cs
+++ Garage2.0/Models/ViewModels/VehicleEditViewModel.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string VehicleType { get; set; }
         public string RegisterNumber { get; set; }
-        public DateTime ArrivalTime { get; set; }
         public string Color { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
