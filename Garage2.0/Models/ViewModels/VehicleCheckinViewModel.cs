052910b418267da56769455c2be6b1186c31769b
Maureen <maureendelafuente.b@gmail.com>
2024-11-01 02:21:37 +0100

    made some strings nullable

--- Garage2.0/Models/ViewModels/VehicleCheckinViewModel.cs
+++ Garage2.0/Models/ViewModels/VehicleCheckinViewModel.cs
@@ -10,19 +10,19 @@ namespace Garage2._0.Models.ViewModels
         public IEnumerable<SelectListItem> VehicleTypes { get; set; } = new List<SelectListItem>();
 
         public int Id { get; set; }
-        public string RegisterNumber { get; set; }
+        public string? RegisterNumber { get; set; }
 
         public VehicleType? VehicleType { get; set; }
 
-        public string Color { get; set; }
+        public string? Color { get; set; }
 
 
         [StringLength(15)]
-        public string Brand { get; set; }
+        public string? Brand { get; set; }
 
 
         [StringLength(16)]
-        public string Model { get; set; }
+        public string? Model { get; set; }
 
         [Range(1,20)]
         public int NumberOfWheels { get; set; }
