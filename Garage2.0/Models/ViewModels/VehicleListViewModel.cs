be34feca9a27503642ad2e26c86b71a2a1137b96
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-01 11:40:52 +0100

    feat: update the vehicle list controller adding  the vehicle types list

--- Garage2.0/Models/ViewModels/VehicleListViewModel.cs
+++ Garage2.0/Models/ViewModels/VehicleListViewModel.cs
@@ -1,14 +1,20 @@
 ﻿using Garage2._0.Models.Entites;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Garage2._0.Models.ViewModels
 {
     public class VehicleListViewModel
+
     {
-        public int Id { get; set; }
+        public IEnumerable<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+        public IEnumerable<SelectListItem> VehicleTypes { get; set; } = new List<SelectListItem>();
+
+        public string? RegisterNumber { get; set; }
         public VehicleType? VehicleType { get; set; }
-        public string RegisterNumber { get; set; }
+        public int Id { get; set; }
         public DateTime ArrivalTime { get; set; }
         public string Color { get; set; }
         public string Brand { get; set; }
+       
     }
 }
