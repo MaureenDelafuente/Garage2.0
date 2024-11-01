0e200c5fbe87b07a620581ca8ac5148c31caa25f
Maureen <maureendelafuente.b@gmail.com>
2024-11-01 02:21:37 +0100

    Added Same validation to VehicleEditVIewModel

--- Garage2.0/Models/ViewModels/VehicleEditViewModel.cs
+++ Garage2.0/Models/ViewModels/VehicleEditViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Garage2._0.Models.Entites;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace Garage2._0.Models.ViewModels
 {
@@ -11,8 +12,16 @@ namespace Garage2._0.Models.ViewModels
         public VehicleType? VehicleType { get; set; }
         public string RegisterNumber { get; set; }
         public string Color { get; set; }
+
+        [StringLength(15)]
         public string Brand { get; set; }
+
+
+        [StringLength(16)]
         public string Model { get; set; }
+
+
+        [Range(1, 20)]
         public int NumberOfWheels { get; set; }
     }
 }
