d795727d35d03f80dd20266700ab5b7822753eee
Maureen <maureendelafuente.b@gmail.com>
2024-11-01 01:24:28 +0100

    Checkin Validation work in progress

--- Garage2.0/Models/ViewModels/VehicleCheckinViewModel.cs
+++ Garage2.0/Models/ViewModels/VehicleCheckinViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Garage2._0.Models.Entites;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using Garage2._0.Models.Entites;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Garage2._0.Models.ViewModels
@@ -14,10 +16,15 @@ namespace Garage2._0.Models.ViewModels
 
         public string Color { get; set; }
 
+
+        [StringLength(15)]
         public string Brand { get; set; }
 
+
+        [StringLength(16)]
         public string Model { get; set; }
 
+        [Range(1,20)]
         public int NumberOfWheels { get; set; }
 
     }
