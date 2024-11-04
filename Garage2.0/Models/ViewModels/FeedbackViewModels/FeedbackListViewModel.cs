8801493dc8f9cfaf441985dc92459717b3d2e4af
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-04 16:26:36 +0100

    fix: css style for selected radio button

---
+++ Garage2.0/Models/ViewModels/FeedbackViewModels/FeedbackListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Garage2._0.Models.Entites;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garage2._0.Models.ViewModels.FeedbackViewModels
+{
+    public class FeedbackListViewModel
+    {
+
+        public int Id { get; set; }
+        public int VehicleId { get; set; }
+        public string Rating { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string FeedbackMessage { get; set; } = string.Empty;
+    }
+}
